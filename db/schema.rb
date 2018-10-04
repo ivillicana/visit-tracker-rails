de36b3b83cb4ccd885bf13829dc0e48170173567
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-10-04 15:49:30 -0400

    create Publication model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_04_194702) do
+ActiveRecord::Schema.define(version: 2018_10_04_194907) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,12 @@ ActiveRecord::Schema.define(version: 2018_10_04_194702) do
     t.text "notes"
     t.string "email"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "publications", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
