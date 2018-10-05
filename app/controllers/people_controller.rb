34f53ae552021deed55e8e8306e15f526fa77b0c
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-10-04 20:54:04 -0400

    add index, show, new, create actions

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,2 +1,31 @@
 class PeopleController < ApplicationController
+ def index
+   @user = current_user
+   @people = @user.people
+ end
+
+ def show
+  if params[:user_id]
+   @user = User.find_by(id: params[:user_id])
+   @person = @user.people.find_by(id: params[:id])
+  else
+    @person = Person.find_by(id: params[:id])
+  end
+ end
+
+ def new
+   @person = Person.new
+ end
+
+ def create
+  @person = Person.create(people_params)
+  @person.user = User.find(params[:user_id])
+  redirect_to user_person_path(@person.user, @person.id) if @person.save
+ end
+
+ private
+
+ def people_params
+   params.require(:person).permit(:name, :house_number, :apt, :street, :city, :state, :country, :notes, :email, :phone)
+ end
 end
