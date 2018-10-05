35ea8d8d368f11603fb0774fa7c9e82f03519447
Isaac Villicana <31767556+ivillicana@users.noreply.github.com>
2018-10-05 11:49:02 -0400

    add #person_address to Person helper

--- app/helpers/person_helper.rb
+++ app/helpers/person_helper.rb
@@ -1,2 +1,11 @@
 module PersonHelper
+  def person_address(person)
+    person.house_number ? (house_number = person.house_number) : (house_number = " ")
+    person.street ? (street = person.street) : (street = " ")
+    person.apt ? (apt = person.apt) : (apt = " ")
+    person.city ? (city = "#{person.city},") : (city = " ")
+    person.state ? (state = person.state) : (state = " ")
+    person.country ? (country = person.country) : (country = " ")
+    raw("#{house_number} #{street}</br>Apt/Floor: #{apt}</br>#{city} #{state} #{country}")
+  end
 end
